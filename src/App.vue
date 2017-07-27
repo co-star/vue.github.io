f833a6b28bb039968f4bfa2dbed5c30183cd3ae1
zhouwei <zhouwei@bubugao.com>
2017-07-27 15:14:41 +0800

    updata

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,16 @@
 <template>
   <div id="app">
-    <h1>{{ title }}</h1>
-    <input v-model="newItem" @keypress.enter="addNew()">
-    <button @click="addNew()">{{buttonText}}</button>
-    <ul v-if="items.length > 0">
-      <h5>todo list</h5>
-      <li v-for="(item,index) in items" v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinish(item)">
-        <button v-on:click.stop="item.quantity += 1" v-show="item.quantity < 5">
-          +
-        </button>
-        {{item.quantity}}
-        <button @click.stop="item.quantity -= 1" v-show="item.quantity > 1">
-          -
-        </button>
-        {{item.label}}
-        <button v-on:click="deleteItem(index)">
-          x
-        </button>
-        <a href="" @click.prevent.stop>{{ love }}</a>
-      </li>
-    </ul>
-    <ul>
-      <li>Total Todos:{{ totalTodo }}<span>Updated:{{ totalTodosUpdated }}</span></li>
-      <li>Total Todo Species:{{ totalTodoSpecie }}<span>Updated:{{ totalTodoSpeciesUpdated }}</span></li>
-    </ul>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Store from './store'
 export default {
-  name: 'app',
-  data: function(){
-    return {
-      title: 'this is a title !',
-      love: 'Todo it !',
-      totalTodosUpdated: 0,
-      totalTodoSpeciesUpdated: 0,
-      buttonText: "Add",
-      items: Store.fetch(),
-      newItem: ''
-    }
-  },
-  methods:{
-    toggleFinish: function(item){
-      item.isFinished = !item.isFinished
-    },
-    addNew: function(){
-      this.items.push({
-        label: this.newItem,
-        isFinished: false,
-        quantity: 1
-      });
-      this.newItem = '';
-    },
-    deleteItem:function(index){
-      this.items.splice(index,1);
-    }
-  },
-  watch:{
-    items:{
-      handler:function(items){
-        Store.save(items);
-      },
-      deep: true
-    }
-  },
-  filter:{
-    capitalize: function(value){
-      if(!value)return'';
-      value = value.toString();
-      return value.charAt(0).toUpperCass()+value.slice(1);
-    },
-    undercass:function(value){
-      if(!value)return'';
-      value = value.toString();
-      return value.tolomerCass();
-    },
-    url:function(value){
-      if(!value)return'';
-      value = value.toString();
-      return "https://www.baidu.com"+value;
-    }
-  },
-  computed:{
-    totalTodo:function(){
-      this.totalTodosUpdated += 1;
-      var sum = 0;
-      var items = this.items;
-      for(var i in items){
-        sum += items[i].quantity;
-      }
-      return sum;
-    },
-    totalTodoSpecie:function(){
-      this.totalTodoSpeciesUpdated += 1;
-      return this.items.length;
-    }
-  }
+  name: 'app'
 }
 </script>
 
 <style>
-.finished{
-  color: #999;
-}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -112,18 +18,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-#app h1{
-  width: 500px;
-  min-height: 100px;
-  margin: 0 auto;
-  color: #fff;
-  background-color: #365;
-}
-#app div{
-  width: 500px;
-  min-height: 100px;
-  margin: 0 auto;
-  color: #000;
 }
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,59 +1,5 @@
-<!-- <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
-</template> -->
-
-// <script>
-// export default {
-//   name: 'hello',
-//   data () {
-//     return {
-//       msg: 'Welcome to Your Vue.js App'
-//     }
-//   }
-// }
-// </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
-
 <template>
-  <div id="app">
+  <div class="hello">
     <h1>{{ title }}</h1>
     <input v-model="newItem" @keypress.enter="addNew()">
     <button @click="addNew()">{{buttonText}}</button>
@@ -84,7 +30,7 @@ a {
 <script>
 import Store from './store'
 export default {
-  name: 'app',
+  name: 'hello',
   data: function(){
     return {
       title: 'this is a title !',
@@ -155,11 +101,12 @@ export default {
 }
 </script>
 
-<style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
 .finished{
   color: #999;
 }
-#app {
+.hello {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -167,14 +114,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-#app h1{
+.hello h1{
   width: 500px;
   min-height: 100px;
   margin: 0 auto;
   color: #fff;
   background-color: #365;
 }
-#app div{
+.hello div{
   width: 500px;
   min-height: 100px;
   margin: 0 auto;
